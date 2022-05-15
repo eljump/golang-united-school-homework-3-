64855a521420201dcba10657b3d36b69c39f3667
elias <eliasejump@gmail.com>
2022-05-15 20:39:16 +0300

    solution

--- task02-slice.go
+++ task02-slice.go
@@ -1,6 +1,13 @@
 package homework
 
 func reverse(input []int64) (result []int64) {
-	//Place your code here
-	return
+
+	res := make([]int64, len(input))
+	j := 0
+	for i := len(input) - 1; i >= 0; i-- {
+		res[j] = input[i]
+		j++
+	}
+
+	return res
 }
--- task03-map.go
+++ task03-map.go
@@ -1,6 +1,21 @@
 package homework
 
+import "sort"
+
 func sortMapValues(input map[int]string) (result []string) {
-	//Place your code here
-	return
+
+	keysSlice := make([]int, 0, len(input))
+	for key, _ := range input {
+		keysSlice = append(keysSlice, key)
+	}
+	sort.Slice(keysSlice, func(i, j int) bool {
+		return keysSlice[i] > keysSlice[j]
+	})
+
+	sorted := make([]string, 0, len(input))
+
+	for _, value := range keysSlice {
+		sorted = append(sorted, input[value])
+	}
+	return sorted
 }
